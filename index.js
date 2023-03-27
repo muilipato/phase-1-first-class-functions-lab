23e6679e5cedf8e64b58168021a7388aa4e3b7ca
Patrick Maweu <60652548+muilipato@users.noreply.github.com>
2023-03-27 11:17:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+  
+  const createFareMultiplier = function (fareMultiplier) {
+    return function(value) {
+      return fareMultiplier * value;
+    }
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  
+  function fetchSpecifiedDrivers(drivers, func) {
+    return func(drivers);
+  } 
+
+  function selectDifferentDrivers(driversArray, funct) {
+    return funct(driversArray);
+} 
